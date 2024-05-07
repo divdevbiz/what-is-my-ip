bc1a77c203692111989f159a3c3920ac90563d46
AyahAnike <143980987+ayahanike@users.noreply.github.com>
2024-05-08 05:42:27 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const script = document.createElement('script');
-script.src = 'https://ipapi.co';
+script.src = 'https://ipapi.co/json/';
 script.crossOrigin = 'anonymous';
 script.access-control-allow-origin = '*';
 document.head.appendChild(script);
