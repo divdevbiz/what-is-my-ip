ab27025f7fd76a9087ef9519fd21f59ef8531a97
AyahAnike <143980987+ayahanike@users.noreply.github.com>
2024-05-08 11:44:52 +0700

    Update main.js

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function setText(obj, text) {
 }
 
 async function init_page() {
-    const localIP = await fetchHandler("/json")
+    const localIP = await fetchHandler("/json/ipdata")
     fillin(localIP, false)
     console.log(localIP)
 }
