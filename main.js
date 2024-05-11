51fe39d788badc9f3bedc0e2c7397909b43673af
AyahAnike <143980987+ayahanike@users.noreply.github.com>
2024-05-11 11:59:53 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const API = "https://ip.hlz.ink"
+const API = "https://ipapi.yoi.workers.dev"
 
 document.addEventListener("DOMContentLoaded", event => {
     init_page()
@@ -13,7 +13,7 @@ function setText(obj, text) {
 }
 
 async function init_page() {
-    const localIP = await fetchHandler("/json")
+    const localIP = await fetchHandler("/json/")
     fillin(localIP, false)
     console.log(localIP)
 }
